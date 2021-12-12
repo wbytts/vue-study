3e7ae0e407e608fb159e41d071e0ebcfd7901ce9
wbytts <wbytts@163.com>
2021-12-12 12:33:29 +0800

    update

--- webpack-vue-demo/src/main.js
+++ webpack-vue-demo/src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
 
+Vue.use(Vuex);
+
+const store = new Vuex.store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        }
+    }
+});
+
 new Vue({
+    store,
     render: h => h(App)
 }).$mount('#root');
