237958b95c43d39be1d370c4c9a434d671b803e9
wangbingyi <1041493283@qq.com>
2022-02-15 17:24:07 +0800

    update

---
+++ vue-cli-demo/src/views/demos/可折叠列表项/demo.vue
@@ -0,0 +1,31 @@
+﻿<template>
+  <div>
+    <FoldItemList :data="list" />
+  </div>
+</template>
+
+<script>
+export const name = 'demo# 列表项可以折叠展开-demo01';
+import FoldItemList from './fold-item-list.vue';
+import { nanoid } from 'nanoid';
+
+export default {
+  components: { FoldItemList },
+  data() {
+    return {
+      list: [
+        { id: nanoid(), title: '1111111', detail: 'detaildetaildetaildetaildetaildetail' },
+        { id: nanoid(), title: '2222222', detail: 'detaildetaildetaildetaildetaildetail' },
+        { id: nanoid(), title: '3333333', detail: 'detaildetaildetaildetaildetaildetail' },
+        { id: nanoid(), title: '4444444', detail: 'detaildetaildetaildetaildetaildetail' },
+        { id: nanoid(), title: '5555555', detail: 'detaildetaildetaildetaildetaildetail' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
+
+
---
+++ vue-cli-demo/src/views/demos/可折叠列表项/fold-item-list.vue
@@ -0,0 +1,45 @@
+﻿<template>
+  <div>
+    <ul class="list">
+      <li v-for="item in data" :key="item.id">
+        <i class="el-icon-caret-bottom arrow" v-show="isFold[item.id]" @click="isFold[item.id]=false"></i>
+        <i class="el-icon-caret-right arrow" v-show="!isFold[item.id]" @click="isFold[item.id]=true"></i>
+        &nbsp;
+        <span>{{item.title}}</span>
+        <div v-show="isFold[item.id]" class="detail">{{item.detail}}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      list: [],
+      isFold: {},
+    };
+  },
+  created() {
+    this.data.forEach(item => {
+      this.isFold = { ...this.isFold, [item.id]: false };
+    });
+  },
+};
+</script>
+
+<style scoped>
+.list .arrow {
+  cursor: pointer;
+}
+
+.detail {
+  padding-left: 20px;
+}
+</style>
+
