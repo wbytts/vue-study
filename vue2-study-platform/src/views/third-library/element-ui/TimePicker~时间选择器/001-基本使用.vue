9eb8d57cce87d45a93a54c1827ce7a0e77f3ce4e
wangbingyi <1041493283@qq.com>
2022-03-18 15:06:27 +0800

    update

---
+++ vue2-study-platform/src/views/third-library/element-ui/TimePicker~时间选择器/001-基本使用.vue
@@ -0,0 +1,28 @@
+﻿<template>
+  <div>
+    <div>
+      <div>value1: {{value1}}</div>
+    </div>
+    <el-time-picker
+      v-model="value1"
+      format="HH:mm"
+      value-format="HH:mm"
+    />
+  </div>
+</template>
+
+<script>
+export const name = 'ElementUI# time-picker# 基本使用'
+
+export default {
+  data() {
+    return {
+      value1: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
